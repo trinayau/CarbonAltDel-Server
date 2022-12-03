fe3a7ab3bca3f9f92cb7eeda5f8c098dd6456534
Trina <92634994+trinayau@users.noreply.github.com>
2022-12-03 16:19:26 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ echo "Collect static files"
 python3.9 manage.py collectstatic --noinput --clear
 
 # echo "create superuser"
-# python3.9 manage.py createsuperuser --noinput
+python3.9 manage.py createsuperuser --noinput
